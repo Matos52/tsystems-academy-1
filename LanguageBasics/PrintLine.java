40987975f1371b28be3822b54556894d7789afce
Matos52 <108865645+Matos52@users.noreply.github.com>
2022-07-14 13:01:22 +0200

    Update PrintLine.java
    Merge commit

--- LanguageBasics/PrintLine.java
+++ LanguageBasics/PrintLine.java
@@ -2,8 +2,8 @@ public class PrintLine {
     public static void main(String[] args) {
         int size = 5;
         for (int i = 0; i < size; i++) {
-            System.out.print("*");
+            System.out.print("*####");
         }
-        System.out.println();
+       System.out.println("****");
     }
 }
