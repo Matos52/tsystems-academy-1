eb848a8e5476b22d8a9ff87c7099b99fa6013168
Matej Regec <Matej.Regec@gmail.com>
2022-07-14 15:29:25 +0200

    14.7.2022

---
+++ UsefulJavaClasses/countries/Country.java
@@ -0,0 +1,12 @@
+package countries;
+
+public class Country {
+
+    private String name;
+    private String shortcut;
+
+    public Country(String name, String shortcut) {
+        this.name = name;
+        this.shortcut = shortcut;
+    }
+}
