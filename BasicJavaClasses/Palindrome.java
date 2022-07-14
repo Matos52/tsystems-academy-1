445d43b245a1338735bab7fcbadd0cc1b736f60b
Matej Regec <Matej.Regec@gmail.com>
2022-07-14 13:02:49 +0200

    Merge

--- BasicJavaClasses/Palindrome.java
+++ BasicJavaClasses/Palindrome.java
@@ -1,26 +1,10 @@
 public class Palindrome {
 	public static void main(String[] args) {
-		String palindrome = "EduaDe";
-		int len = palindrome.length();
-		StringBuilder dest = new StringBuilder(len);
-		for (int i = (len - 1); i >= 0; i--) {
-			dest.append(palindrome.charAt(i));
-		}
-		boolean check = true;
-		int counter = 0;
-		while(check) {
-			if(dest.toString().toLowerCase().charAt(counter) == palindrome.toLowerCase().charAt(counter)) {
-				System.out.println(dest.toString().charAt(counter));
-				System.out.println(palindrome.charAt(counter));
-				counter++;
-				if(counter == palindrome.length()) {
-					System.out.println("Su rovnake");
-					break;
-				}
-			} else {
-				System.out.println("Niesu rovnake");
-				break;
-			}
+		StringBuilder dest = new StringBuilder("Matej a Jetam");
+		if(dest.toString().equalsIgnoreCase(dest.reverse().toString())) {
+			System.out.println("Su rovnake");
+		} else {
+			System.out.println("Niesu rovnake");
 		}
 	}	
 }
--- LanguageBasics/PrintSquare.java
+++ LanguageBasics/PrintSquare.java
@@ -3,9 +3,11 @@ public class PrintSquare {
         int size = 5;
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
-                System.out.print("*");
+                System.out.print("*****");
             }
             System.out.println();
         }
     }
 }
+
+//komentar skuska
