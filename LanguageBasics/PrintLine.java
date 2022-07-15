e16d622f7f4ca3571527a35d6e97e1e01298ff86
Regec <Matej.Regec@telekom.com>
2022-07-15 14:56:37 +0200

    15.7.2022

--- LanguageBasics/PrintLine.java
+++ LanguageBasics/PrintLine.java
@@ -1,12 +1,9 @@
 public class PrintLine {
     public static void main(String[] args) {
-        int size = 5;
-        for (int i = 0; i < size; i++) {
-            System.out.print("*####");
+            int size = 5;
+            for (int i = 0; i < size; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
-        System.out.println("-----");
     }
-}
-
-//koment
