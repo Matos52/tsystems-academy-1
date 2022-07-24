5e2868664635e3987f02a07da67beba3b7d522ee
Regec <Matej.Regec@telekom.com>
2022-07-24 21:01:52 +0200

    24.7.2022

--- IO/io/ListDir.java
+++ IO/io/ListDir.java
@@ -1,35 +1,97 @@
 package io;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.*;
 
 public class ListDir {
     public static void main(String[] args) {
-        File dir = new File(".");
+        List<File> filteredByName = filterByName("./arrays", "Array");
+        List<File> filteredByDate = filterByDate("./arrays", new GregorianCalendar(2022, Calendar.JULY, 21).getTime());
+//        List<File> filteredBinFiles = fiterBinFiles(".");
+        System.out.println("By name:");
+        list(filteredByName);
+        System.out.println("By date:");
+        list(filteredByDate);
+//        list(filteredBinFiles);
+    }
 
-        if (dir.isDirectory()) {
-            System.out.println("Listing of: " + dir.getAbsolutePath());
+    private static void listDir(String fileName) {
+        listDir(new File(fileName));
+    }
 
-            //List directory
-            for (String fileName : dir.list()) {
-                System.out.println("  " + fileName);
+    private static void list(List<File> files) {
+        //vypisat zoznam suborov
+        //pouzit stream a forEach
+        files.stream().forEach(file -> listDir(file));
+    }
+
+    private static void listDir(File f) {
+        System.out.println("  " + f.getName());
+        if (f.isDirectory()) {
+            for (File sub : f.listFiles()) {
+                listDir(sub);
             }
-        } else {
-            System.err.printf("File %s is not directory", dir);
         }
     }
 
-    private static void listDir(String path) {
-        File dir = new File(".");
-        System.out.println();
-        if (dir.isDirectory()) {
-            System.out.println("Listing of: " + dir.getAbsolutePath());
+    private static List<File> fiterBinFiles(String path) {
+        List<File> filtered = new ArrayList<>();
+        File dir = new File(path);
 
-            //List directory
-            for (String fileName : dir.list()) {
-                System.out.println("  " + fileName);
+        FileFilter binFileFilter = f -> f.getName().endsWith(".bin");
+
+        filterUsingFileFilter(dir, binFileFilter, filtered);
+        return filtered;
+    }
+
+    private static List<File> filterByName(String path, String name) {
+        List<File> filtered = new ArrayList<>();
+        File dir = new File(path);
+//        FileFilter dateFileFilter = new FileFilter() {
+//            @Override
+//            public boolean accept(File f) {
+//                return f.getName().contains(name);
+//            }
+//        };
+
+        FileFilter nameFileFilter = f -> f.getName().contains(name);
+
+        filterUsingFileFilter(dir, nameFileFilter, filtered);
+        return filtered;
+    }
+
+    private static List<File> filterByDate(String path, Date maxDate) {
+        List<File> filtered = new ArrayList<>();
+        File dir = new File(path);
+//        FileFilter dateFileFilter = new FileFilter() {
+//            @Override
+//            public boolean accept(File f) {
+//                return new Date(f.lastModified()).after(maxDate);
+//            }
+//        };
+
+        FileFilter dateFileFilter = f -> new Date(f.lastModified()).after(maxDate);
+
+        filterUsingFileFilter(dir, dateFileFilter, filtered);
+        return filtered;
+    }
+
+    private static void filterUsingFileFilter(File f, FileFilter filter, List<File> filtered) {
+        if(f.isDirectory()) {
+            filtered.addAll(Arrays.asList(f.listFiles(filter)));
+            for(File sub : f.listFiles()) {
+                filterUsingFileFilter(sub, filter, filtered);
             }
+        }
+    }
+
+    private static void filterRecursive(File f, List<File> filtered) {
+        if (!f.isDirectory()) {
+            filtered.add(f);
         } else {
-            System.err.printf("File %s is not directory", dir);
+            for (File sub : f.listFiles())
+                filterRecursive(sub, filtered);
         }
     }
 }
--- UsefulJavaClasses/regex/RegexTest.java
+++ UsefulJavaClasses/regex/RegexTest.java
@@ -7,18 +7,17 @@ import java.util.regex.Matcher;
 public class RegexTest {
 
     public static void main(String[] args){
-        Pattern pattern =
-                Pattern.compile("(?:\\+|00)(\\d{1,3}) (\\(\\d{3}\\)|\\d{3})(( \\d{2}){3}|( \\d{3}){2})");
-        Matcher matcher =
-                pattern.matcher("+55 123 123 123");
+        Pattern pattern = Pattern.compile("(?:\\+|00)(\\d{1,3}) (\\(\\d{3}\\)|\\d{3})(( \\d{2}){3}|( \\d{3}){2})");
+        Matcher matcher = pattern.matcher("+55 123 123 123");
 
-        matcher.matches();
-
-        System.out.println(matcher.group(0));
-        System.out.println(matcher.group(1));
+        boolean checker = matcher.matches();
+        if(checker) {
+            System.out.println(matcher.group(0));
+            System.out.println(matcher.group(1));
 //        matcher.groupCount();
-        for (int g = 0; g < matcher.groupCount(); g++) {
-            System.out.println(matcher.group(g));
+            for (int g = 0; g < matcher.groupCount(); g++) {
+                System.out.println(matcher.group(g));
+            }
         }
     }
 }
